e4b45db6aabb0a61249ecd3033e51b38afc078f8
sudarshanrbhat.srb2 <sudarshanrbhat.srb2@gmail.com>
2020-09-14 22:14:07 +0530

    components generated

--- Musix-App/src/app/app.module.ts
+++ Musix-App/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { FooterComponent } from './footer/footer.component';
+import { SearchResultComponent } from './search-result/search-result.component';
+import { SongsComponent } from './songs/songs.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    DashboardComponent,
+    FooterComponent,
+    SearchResultComponent,
+    SongsComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
