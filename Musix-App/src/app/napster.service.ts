5b08e799af561dea602f6d953d7343b44bc8266b
sudarshanrbhat.srb2 <sudarshanrbhat.srb2@gmail.com>
2020-09-17 09:57:17 +0530

    "updated"

--- Musix-App/src/app/napster.service.ts
+++ Musix-App/src/app/napster.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NapsterService {
 
-  constructor() { }
+  apiKey = '';
+  constructor(private http: HttpClient) { }
+
+  fetchTopArtists() {
+    // api key:    
+    // api secret:    
+    // callback url:   musix-repo
+    return this.http.get<any>("http://api.napster.com/v2.2/artists/top?apikey=" + this.apiKey);
+  }
+
 }
--- Musix-App/src/app/search-result/search-result.component.ts
+++ Musix-App/src/app/search-result/search-result.component.ts
@@ -1,5 +1,6 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-
+import { NapsterService } from '../napster.service';
 @Component({
   selector: 'app-search-result',
   templateUrl: './search-result.component.html',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchResultComponent implements OnInit {
 
-  constructor() { }
+  constructor(private napsterService: NapsterService) { }
 
+  artists;
   ngOnInit(): void {
+    this.fetchTopArtists();
+  }
+
+  fetchTopArtists() {
+    this.napsterService
+    .fetchTopArtists()
+    .subscribe(
+      res => this.artists = res, 
+      err => err == HttpErrorResponse ? console.log(err): console.log("something went wrong")
+      );    
   }
 
 }
